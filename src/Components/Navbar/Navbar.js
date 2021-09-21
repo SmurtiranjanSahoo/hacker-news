4fbd6cce74f358a14aaaed5313e5b2f368a9402e
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-21 11:00:02 +0530

    refactor: links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,86 @@
 import React from "react";
 import "./Navbar.css";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <div className="navbar-container">
       <div className="navbar-primary">
-        <span className="navbar-logo">
+        <Link to="/" className="navbar-logo">
           <img src="https://news.ycombinator.com/y18.gif" alt="" />
-        </span>
+        </Link>
         <span className="navbar-primary-span">
-          <span className="navbar-title">Hacker News</span>
-          <span>new | </span>
-          <span>past | </span>
-          <span>comments | </span>
-          <span>ask | </span>
-          <span>show | </span>
-          <span>jobs | </span>
-          <span>submit</span>
+          <Link to="/" className="navbar-title">
+            Hacker News
+          </Link>
+          <Link to="/new">
+            <span
+              style={{
+                color: pathname === "/new" ? "#ffffff" : "000000",
+              }}
+            >
+              new
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/past">
+            <span
+              style={{
+                color: pathname === "/past" ? "#ffffff" : "000000",
+              }}
+            >
+              past
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/comments">
+            <span
+              style={{
+                color: pathname === "/comments" ? "#ffffff" : "000000",
+              }}
+            >
+              comments
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/ask">
+            <span
+              style={{
+                color: pathname === "/ask" ? "#ffffff" : "000000",
+              }}
+            >
+              ask
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/show">
+            <span
+              style={{
+                color: pathname === "/show" ? "#ffffff" : "000000",
+              }}
+            >
+              show
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/jobs">
+            <span
+              style={{
+                color: pathname === "/jobs" ? "#ffffff" : "000000",
+              }}
+            >
+              jobs
+            </span>{" "}
+            |{" "}
+          </Link>
+          <Link to="/submit">submit</Link>
         </span>
       </div>
       <div className="navbar-secondary">
-        <p>login</p>
+        <Link to="/login">login</Link>
       </div>
     </div>
   );
