643019896023edcec454d0727c9264ea156ce705
Smurtiranjan Sahoo <rajsahoo976@gmail.com>
2021-09-21 19:25:46 +0530

    feat: add responsiveness

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -16,67 +16,69 @@ const Navbar = () => {
           <Link to="/" className="navbar-title">
             Hacker News
           </Link>
-          <Link to="/new">
-            <span
-              style={{
-                color: pathname === "/new" ? "#ffffff" : "000000",
-              }}
-            >
-              new
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/past">
-            <span
-              style={{
-                color: pathname === "/past" ? "#ffffff" : "000000",
-              }}
-            >
-              past
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/comments">
-            <span
-              style={{
-                color: pathname === "/comments" ? "#ffffff" : "000000",
-              }}
-            >
-              comments
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/ask">
-            <span
-              style={{
-                color: pathname === "/ask" ? "#ffffff" : "000000",
-              }}
-            >
-              ask
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/show">
-            <span
-              style={{
-                color: pathname === "/show" ? "#ffffff" : "000000",
-              }}
-            >
-              show
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/jobs">
-            <span
-              style={{
-                color: pathname === "/jobs" ? "#ffffff" : "000000",
-              }}
-            >
-              jobs
-            </span>{" "}
-            |{" "}
-          </Link>
-          <Link to="/submit">submit</Link>
+          <span className="navbar-primary-span-1">
+            <Link to="/new">
+              <span
+                style={{
+                  color: pathname === "/new" ? "#ffffff" : "000000",
+                }}
+              >
+                new
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/past">
+              <span
+                style={{
+                  color: pathname === "/past" ? "#ffffff" : "000000",
+                }}
+              >
+                past
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/comments">
+              <span
+                style={{
+                  color: pathname === "/comments" ? "#ffffff" : "000000",
+                }}
+              >
+                comments
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/ask">
+              <span
+                style={{
+                  color: pathname === "/ask" ? "#ffffff" : "000000",
+                }}
+              >
+                ask
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/show">
+              <span
+                style={{
+                  color: pathname === "/show" ? "#ffffff" : "000000",
+                }}
+              >
+                show
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/jobs">
+              <span
+                style={{
+                  color: pathname === "/jobs" ? "#ffffff" : "000000",
+                }}
+              >
+                jobs
+              </span>{" "}
+              |{" "}
+            </Link>
+            <Link to="/submit">submit</Link>
+          </span>
         </span>
       </div>
       <div className="navbar-secondary">
